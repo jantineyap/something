9bb03c776fe6db73ca0b7c0bf4f4103d9bded606
Sarthak <smohapatra30@gatech.edu>
2015-02-10 17:31:47 -0500

    edit

--- app/src/main/java/com/teamanything/goonsquad/RegistrationActivity.java
+++ app/src/main/java/com/teamanything/goonsquad/RegistrationActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class RegistrationActivity extends ActionBarActivity {
 
     private static final String PREFS_NAME = "LoginInfo";
