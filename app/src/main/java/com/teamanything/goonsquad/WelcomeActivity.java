00f5208c610f510596ddd2d176b6d0562588235f
david <dcarroll33@gatech.edu>
2015-02-13 02:55:40 -0500

    comments/javadocs

--- app/src/main/java/com/teamanything/goonsquad/WelcomeActivity.java
+++ app/src/main/java/com/teamanything/goonsquad/WelcomeActivity.java
@@ -12,29 +12,52 @@ public class WelcomeActivity extends ActionBarActivity {
 
 
 
-    @Override
+    /**
+     * Creates the view and layout for the Welcome Activity
+     * @param savedInstanceState for the created activity
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //selects XML file for layout of the welcome activity screen
         setContentView(R.layout.activity_welcome);
     }
 
+    /**
+     * Opens the activity for logging in
+     * @param view for activity
+     */
     public void openLoginActivity(View view) {
+        //begins the login activity
         startActivity(new Intent(this, LoginActivity.class));
     }
 
+    /**
+     * Opens the activity for registration on the welcome screen
+     * @param view for activity
+     */
     public void openRegistrationActivity(View view) {
         startActivity(new Intent(this, RegistrationActivity.class));
     }
 
 
-    @Override
+    /**
+     * Places menu items in the action bar
+     * @param menu where items are to place into action bar
+     * @return boolean true if successful
+     */
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_welcome, menu);
         return true;
     }
 
-    @Override
+    /**
+     * Called when user presses action button and determines which
+     * item is pressed
+     * @param item from the menu that is selected
+     * @return boolean true if id matches value declared in the
+     * corresponding item's action settings
+     */
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
