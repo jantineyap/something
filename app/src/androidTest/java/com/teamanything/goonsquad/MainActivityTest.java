3562351f87db8f7fe5758cac5d5151e407ec0c19
Adam <asasine@gatech.edu>
2015-04-02 18:46:05 -0400

    More JUnits

--- app/src/androidTest/java/com/teamanything/goonsquad/MainActivityTest.java
+++ app/src/androidTest/java/com/teamanything/goonsquad/MainActivityTest.java
@@ -4,6 +4,8 @@ import android.test.ActivityInstrumentationTestCase2;
 
 /**
  * Created by Adam on 4/2/2015.
+ *
+ * Test cases for MainActivity
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
--- app/src/androidTest/java/com/teamanything/goonsquad/WishListItemAdapterTest.java
+++ app/src/androidTest/java/com/teamanything/goonsquad/WishListItemAdapterTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * Created by Adam on 4/2/2015.
  *
- * JUnit Test class for Adapter
+ * JUnit Test class for WishListItemAdapter
  */
 public class WishListItemAdapterTest extends AndroidTestCase {
 
@@ -21,10 +21,9 @@ public class WishListItemAdapterTest extends AndroidTestCase {
     protected void setUp() throws Exception {
         super.setUp();
         data = new ArrayList<>();
-        data.add(new WishListItem("1", 1));
-        data.add(new WishListItem("2", 2));
-        data.add(new WishListItem("3", 3));
-
+        for (int i = 1; i <= 5; i++) {
+            data.add(new WishListItem(Integer.toString(i), i));
+        }
         adapter = new WishListItemAdapter(getContext(), data);
     }
 
@@ -34,8 +33,20 @@ public class WishListItemAdapterTest extends AndroidTestCase {
         data.clear();
     }
 
+    /**
+     * Tests for proper count
+     */
     public void testCount() {
-        assertEquals(3, adapter.getCount());
+        assertEquals(5, adapter.getCount());
+    }
+
+    /**
+     * Tests if WishListItem are stored in correct order with correct price
+     */
+    public void testMaxPrice() {
+        for (int i = 0; i < adapter.getCount(); i++) {
+            assertEquals((double) (i + 1), adapter.getItem(i).getMaxPrice());
+        }
     }
 
 }
